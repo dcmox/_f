c8ad4568341263acd283f53c2ffc07d0dbca11b0
dcmox <dancmox@comcast.net>
2020-02-05 07:59:52 -0600

    fixed test

--- tests/_s.test.ts
+++ tests/_s.test.ts
@@ -206,11 +206,11 @@ describe('_s test suite', () => {
     it('should right shift a string', () => {
         const tests: any = [
             { s: '1231', times: 3, expected: '0001'},
-            { s: '54321', times: 4, expected: '00001' },
-            { s: '1world', times: 5, expected: '00000'},
+            { s: '54321', times: 4, expected: '00005' },
+            { s: '1world', times: 5, expected: '000000'},
         ]
         tests.forEach((test: any) => {
-            assert.equal(_s.rightRotate(test.s, test.times), test.expected)
+            assert.equal(_s.rightShift(test.s, test.times), test.expected)
         })
     })
 })
